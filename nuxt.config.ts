b5df44274cfe9c3a0beef863013e4cfe7904a381
Reptilz <jmorlet94@gmail.com>
2025-08-02 23:30:12 +0200

    fix: config netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  nitro: {
-    preset: 'netlify'
-  },
   vite: {
     plugins: [
       tailwindcss(),
